b879c6b98753d735389bba52c4a3fcef3e0bab85
gustavo.gomes <gustavo.gomes@conveste.com.br>
2021-05-13 16:13:07 -0300

    criando os componentes dash/header/TransactionsTable/Summary

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,5 +5,28 @@ export const Container = styled.header`
 `;
 
 export const Content = styled.div`
-    background: var(--blue);
+    max-width: 1120px;
+    margin: 0 auto;
+
+    padding: 2rem 1rem 12rem;
+    display: flex; 
+    align-items: center;
+    justify-content: space-between;
+
+    button {
+        font-size: 1rem;
+        color: #FFF;
+        background: var(--blue-light);
+        border: 0;
+        padding: 0 2rem;
+        border-radius: 0.25rem;
+        height: 3rem;
+
+        transition: filter 0.2s;
+
+        &:hover {
+            filter: brightness(0.9)
+            // fazendo isso o botão recebe uma leve "escurecida"
+        }
+    }
 `;
