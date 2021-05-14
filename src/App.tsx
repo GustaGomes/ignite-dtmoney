d4c1ad85fa2c5ece8c3cfeb36b57d45a30e80fab
gustavo.gomes <gustavo.gomes@conveste.com.br>
2021-05-14 17:06:22 -0300

    configurando uma resp fake api / modal global no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-import styled from 'styled-components'
+import Modal from 'react-modal';
+import { useState } from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
-import { GlobalStyle } from './styles/global'
+import { GlobalStyle } from './styles/global';
 
-const Title = styled.h1`
-
-`
+Modal.setAppElement('#root');
 
 export function App() {
+  
+  const [ isNewTransactionOpen, setIsNewTransactionOpen ] = useState( false );
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal 
+          isOpen={isNewTransactionOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+      >
+          <h1>Aeee uhuu, passei por aqui no modal, vamo que vamo</h1>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
