c3ae9b49f4715085f81106dc18a9dbc5e1ffdc4b
gustavo.gomes <gustavo.gomes@conveste.com.br>
2021-05-17 18:23:01 -0300

    colocando estilo no input

--- src/components/NewTransactionModal/style.ts
+++ src/components/NewTransactionModal/style.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
-    h2{
+    h2 {
         color: var(--text-title);
         font-size: 1.5rem;
         margin-bottom: 2rem;
@@ -39,6 +39,7 @@ export const Container = styled.form`
         border: 0;
         font-size: 1rem;
         margin-top: 1.5rem;
+        font-weight: 600;
 
         transition: 0.2s;
 
@@ -53,35 +54,44 @@ export const TransactionTypeContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
+`;
 
-    button {
-        height: 4rem;
-        border: 1px solid #d7d7d7;
-        border-radius: 0.25rem;
+interface RadioBoxProps {
+    isActive: boolean;
+    activeColor: 'green' | 'red';
+}
 
-        background: transparent;
+const colors = {
+    green: '#33CC95',
+    red: '#E52E4D',
+}
 
-        display: flex;
-        align-items: center;
-        justify-content: center;
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
 
-        transition: border-color 0.2s;
+    background:${(props) => props.isActive ? transparentize(0.9 , colors[props.activeColor]) : 'transparent'};
 
-        &:hover {
-            border-color: ${darken(0.1, '#d7d7d7')};
-        }
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-        img{
-            width:20px;
-            height: 20px;
-        }
+    transition: border-color 0.2s;
 
-        span {
-            display: inline-block;
-            margin-left: 1rem;
-            font-size: 1rem;
-            color: var(--text-title);
-        }
+    &:hover {
+        border-color: ${darken(0.1, '#d7d7d7')};
     }
 
+    img{
+        width:20px;
+        height: 20px;
+    }
+
+    span {
+        display: inline-block;
+        margin-left: 1rem;
+        font-size: 1rem;
+        color: var(--text-title);
+    }
 `;
